c5e0ea4d29d52b028390101f1a551180fb785d31
Sumen113 <Sumen113k@gmail.com>
2025-04-17 22:16:59 +0930

    nowayy

--- public/example.js
+++ public/example.js
@@ -1,8 +1,11 @@
-document.getElementById("urlInput").addEventListener("keydown", function (event) {
+const urlInput = document.getElementById("urlInput");
+const toggleIcon = document.querySelector(".toggle-icon");
+
+urlInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
 
-        let url = document.getElementById("urlInput").value;
+        let url = urlInput.value;
         let searchUrl = "https://www.google.com/search?q=";
 
         if (!url.includes(".")) {
@@ -14,5 +17,15 @@ document.getElementById("urlInput").addEventListener("keydown", function (event)
         }
 
         iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+
+        // Hide input and show toggle arrow
+        urlInput.style.display = "none";
+        toggleIcon.style.display = "block";
     }
+});
+
+toggleIcon.addEventListener("click", () => {
+    // Show input and hide arrow
+    urlInput.style.display = "block";
+    toggleIcon.style.display = "none";
 });
